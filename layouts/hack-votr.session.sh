5ea6fccf16677a78d41a7e3123d8e0ddda86d9d7
Maksim Pecherskiy <max@maksimize.com>
2014-04-23 18:37:10 -0700

    Adding session layouts for hack projects.

---
+++ layouts/hack-votr.session.sh
@@ -0,0 +1,24 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/Code"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "hack-votr"; then
+
+  # Create a new window inline within session layout definition.
+  #new_window "misc"
+
+  # Load a defined window layout.
+  load_window "hack-votr"
+  load_window "hack-starter"
+  load_window "mongo"
+  new_window "shell"
+
+  # Select the default active window on session creation.
+  #select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
